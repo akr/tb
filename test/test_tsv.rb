dc7b7fd8fc9d5700611551bb21d968aaa9209d4f
Tanaka Akira <akr@fsij.org>
2010-12-07 13:42:57 +0900

    add a test.

--- test/test_tsv.rb
+++ test/test_tsv.rb
@@ -13,4 +13,23 @@ class TestTableTSV < Test::Unit::TestCase
       [{"_recordid"=>0, "a"=>"1", "b"=>"2"}],
       records)
   end
+
+  def test_parse_conv
+    tsv = "foo\na\tb\n1\t2\n"
+    t = Table.parse_tsv(tsv) {|aa|
+      assert_equal([%w[foo],
+                   %w[a b],
+                   %w[1 2]],
+                   aa)
+      aa.shift
+      aa
+    }
+    records = []
+    t.each_record {|record|
+      records << record.to_h
+    }
+    assert_equal(
+      [{"_recordid"=>0, "a"=>"1", "b"=>"2"}],
+      records)
+  end
 end
