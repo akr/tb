d1d618508c6f90a355b3d22d4d5f1ecc0c0ab538
Tanaka Akira <akr@fsij.org>
2011-03-08 01:54:13 +0900

    make State class to wrap pos and cap.

--- lib/table/pathfinder.rb
+++ lib/table/pathfinder.rb
@@ -47,8 +47,8 @@ module Table::Pathfinder
   def each_match(pat, aa, spos=nil)
     if spos
       run {
-        try(pat, aa, spos, {}.freeze) {|epos, cap2|
-          yield spos, epos, cap2
+        try(pat, aa, State.new(spos, {}.freeze)) {|st2|
+          yield spos, st2.pos, st2.store
           nil
         }
       }
@@ -57,8 +57,8 @@ module Table::Pathfinder
         a.each_index {|x|
           spos = [x,y]
           run {
-            try(pat, aa, spos, {}.freeze) {|epos, cap2|
-              yield spos, epos, cap2
+            try(pat, aa, State.new(spos, {}.freeze)) {|st2|
+              yield spos, st2.pos, st2.store
               nil
             }
           }
@@ -88,40 +88,40 @@ module Table::Pathfinder
     end
   end
 
-  def try(pat, aa, pos, cap, &b)
+  def try(pat, aa, st, &b)
     case pat
-    when nil; lambda { yield pos, cap }
-    when String; try_lit(pat, aa, pos, cap, &b)
-    when Regexp; try_regexp(pat, aa, pos, cap, &b)
-    when :n, :north; try_rmove(:north, aa, pos, cap, &b)
-    when :s, :south; try_rmove(:south, aa, pos, cap, &b)
-    when :e, :east; try_rmove(:east, aa, pos, cap, &b)
-    when :w, :west; try_rmove(:west, aa, pos, cap, &b)
-    when :debug_print_state; p [pos, cap]; yield pos, cap
+    when nil; lambda { yield st }
+    when String; try_lit(pat, aa, st, &b)
+    when Regexp; try_regexp(pat, aa, st, &b)
+    when :n, :north; try_rmove(:north, aa, st, &b)
+    when :s, :south; try_rmove(:south, aa, st, &b)
+    when :e, :east; try_rmove(:east, aa, st, &b)
+    when :w, :west; try_rmove(:west, aa, st, &b)
+    when :debug_print_state; p st; yield st
     when Array
       case pat[0]
-      when :rmove; _, dir = pat; try_rmove(dir, aa, pos, cap, &b)
-      when :lit; _, val = pat; try_lit(val, aa, pos, cap, &b)
-      when :regexp; _, re = pat; try_regexp(re, aa, pos, cap, &b)
-      when :cat; _, *ps = pat; try_cat(ps, aa, pos, cap, &b)
-      when :alt; _, *ps = pat; try_alt(ps, aa, pos, cap, &b)
-      when :rep; _, *ps = pat; try_rep_generic(nil, 0, nil, true, ps, aa, pos, cap, &b)
-      when :rep1; _, *ps = pat; try_rep_generic(nil, 1, nil, true, ps, aa, pos, cap, &b)
-      when :rep_nongreedy; _, *ps = pat; try_rep_generic(nil, 0, nil, false, ps, aa, pos, cap, &b)
-      when :rep1_nongreedy; _, *ps = pat; try_rep_generic(nil, 1, nil, false, ps, aa, pos, cap, &b)
-      when :opt; _, *ps = pat; try_rep_generic(nil, 0, 1, true, ps, aa, pos, cap, &b)
-      when :opt_nongreedy; _, *ps = pat; try_rep_generic(nil, 0, 1, false, ps, aa, pos, cap, &b)
-      when :repn; _, num, *ps = pat; try_rep_generic(nil, num, num, true, ps, aa, pos, cap, &b)
-      when :repeat; _, var, min, max, greedy, *ps = pat; try_rep_generic(var, min, max, greedy, ps, aa, pos, cap, &b)
-      when :grid; _, *arys = pat; try_grid(arys, aa, pos, cap, &b)
-      when :capval; _, n = pat; try_capval(n, aa, pos, cap, &b)
-      when :refval; _, n = pat; try_refval(n, aa, pos, cap, &b)
-      when :tmp_pos; _, dx, dy, *ps = pat; try_tmp_pos(dx, dy, ps, aa, pos, cap, &b)
-      when :save_pos; _, n = pat; try_save_pos(n, aa, pos, cap, &b)
-      when :push_pos; _, n = pat; try_push_pos(n, aa, pos, cap, &b)
-      when :pop_pos; _, n = pat; try_pop_pos(n, aa, pos, cap, &b)
-      when :update; _, pr = pat; try_update(pr, aa, pos, cap, &b)
-      when :assert; _, pr = pat; try_assert(pr, aa, pos, cap, &b)
+      when :rmove; _, dir = pat; try_rmove(dir, aa, st, &b)
+      when :lit; _, val = pat; try_lit(val, aa, st, &b)
+      when :regexp; _, re = pat; try_regexp(re, aa, st, &b)
+      when :cat; _, *ps = pat; try_cat(ps, aa, st, &b)
+      when :alt; _, *ps = pat; try_alt(ps, aa, st, &b)
+      when :rep; _, *ps = pat; try_rep_generic(nil, 0, nil, true, ps, aa, st, &b)
+      when :rep1; _, *ps = pat; try_rep_generic(nil, 1, nil, true, ps, aa, st, &b)
+      when :rep_nongreedy; _, *ps = pat; try_rep_generic(nil, 0, nil, false, ps, aa, st, &b)
+      when :rep1_nongreedy; _, *ps = pat; try_rep_generic(nil, 1, nil, false, ps, aa, st, &b)
+      when :opt; _, *ps = pat; try_rep_generic(nil, 0, 1, true, ps, aa, st, &b)
+      when :opt_nongreedy; _, *ps = pat; try_rep_generic(nil, 0, 1, false, ps, aa, st, &b)
+      when :repn; _, num, *ps = pat; try_rep_generic(nil, num, num, true, ps, aa, st, &b)
+      when :repeat; _, var, min, max, greedy, *ps = pat; try_rep_generic(var, min, max, greedy, ps, aa, st, &b)
+      when :grid; _, *arys = pat; try_grid(arys, aa, st, &b)
+      when :capval; _, n = pat; try_capval(n, aa, st, &b)
+      when :refval; _, n = pat; try_refval(n, aa, st, &b)
+      when :tmp_pos; _, dx, dy, *ps = pat; try_tmp_pos(dx, dy, ps, aa, st, &b)
+      when :save_pos; _, n = pat; try_save_pos(n, aa, st, &b)
+      when :push_pos; _, n = pat; try_push_pos(n, aa, st, &b)
+      when :pop_pos; _, n = pat; try_pop_pos(n, aa, st, &b)
+      when :update; _, pr = pat; try_update(pr, aa, st, &b)
+      when :assert; _, pr = pat; try_assert(pr, aa, st, &b)
       else raise "unexpected: #{pat.inspect}"
       end
     else
@@ -129,55 +129,55 @@ module Table::Pathfinder
     end
   end
 
-  def try_rmove(dir, aa, pos, cap)
-    x, y = pos
+  def try_rmove(dir, aa, st)
+    x, y = st.pos
     case dir
     when :east then x += 1
     when :west then x -= 1
     when :north then y -= 1
     when :south then y += 1
     end
-    lambda { yield [x,y], cap }
+    lambda { yield State.new([x,y], st.store) }
   end
 
-  def try_lit(val, aa, pos, cap)
-    x, y = pos
+  def try_lit(val, aa, st)
+    x, y = st.pos
     if 0 <= y && y < aa.length &&
        0 <= x && x < aa[y].length &&
        aa[y][x] == val
-      lambda { yield pos, cap }
+      lambda { yield st }
     else
       nil
     end
   end
 
-  def try_regexp(re, aa, pos, cap)
-    x, y = pos
+  def try_regexp(re, aa, st)
+    x, y = st.pos
     if 0 <= y && y < aa.length &&
        0 <= x && x < aa[y].length &&
        re =~ aa[y][x]
-      lambda { yield pos, cap }
+      lambda { yield st }
     else
       nil
     end
   end
 
   # p1 p2 ...
-  def try_cat(ps, aa, pos, cap, &block)
+  def try_cat(ps, aa, st, &block)
     if ps.empty?
-      lambda { yield pos, cap }
+      lambda { yield st }
     else
       pat, *rest = ps
-      try(pat, aa, pos, cap) {|pos2, cap2|
-        try_cat(rest, aa, pos2, cap2, &block)
+      try(pat, aa, st) {|st2|
+        try_cat(rest, aa, st2, &block)
       }
     end
   end
 
   # p1 | p2 | ...
-  def try_alt(ps, aa, pos, cap, &block)
+  def try_alt(ps, aa, st, &block)
     ps.map {|pat|
-      lambda { try(pat, aa, pos, cap, &block) }
+      lambda { try(pat, aa, st, &block) }
     }
   end
 
@@ -187,28 +187,25 @@ module Table::Pathfinder
   # (p1 p2 ...)*?
   # (p1 p2 ...){min,}?
   # (p1 p2 ...){min,max}?
-  def try_rep_generic(var, min, max, greedy, ps, aa, pos, cap, visited={}, &block)
-    min = cap[min].to_int if Symbol === min
-    max = cap[max].to_int if Symbol === max
+  def try_rep_generic(var, min, max, greedy, ps, aa, st, visited={}, &block)
+    min = st[min].to_int if Symbol === min
+    max = st[max].to_int if Symbol === max
     visited2 = visited.dup
-    visited2[pos] = true
+    visited2[st.pos] = true
     result = []
     if min <= 0 && !greedy
       result << lambda { 
-        if var
-          cap = cap.dup
-          cap[var] = visited.size
-        end
-        yield pos, cap
+        st = st.merge(var => visited.size) if var
+        yield st
       }
     end
     if max.nil? || 0 < max
       min2 = min <= 0 ? 0 : min-1
       max2 = max ? max-1 : nil
       result << lambda {
-        try_cat(ps, aa, pos, cap) {|pos2, cap2|
-          if !visited2[pos2]
-            try_rep_generic(var, min2, max2, greedy, ps, aa, pos2, cap2, visited2, &block)
+        try_cat(ps, aa, st) {|st2|
+          if !visited2[st2.pos]
+            try_rep_generic(var, min2, max2, greedy, ps, aa, st2, visited2, &block)
           else
             nil
           end
@@ -217,17 +214,14 @@ module Table::Pathfinder
     end
     if min <= 0 && greedy
       result << lambda {
-        if var
-          cap = cap.dup
-          cap[var] = visited.size
-        end
-        yield pos, cap
+        st = st.merge(var => visited.size) if var
+        yield st
       }
     end
     result
   end
 
-  def try_grid(arys, aa, pos, cap)
+  def try_grid(arys, aa, st)
     start = nil
     goal = nil
     newarys = []
@@ -267,113 +261,142 @@ module Table::Pathfinder
     }
     raise ArgumentError, "no start" if !start
     raise ArgumentError, "no goal" if !goal
-    try_grid_rect(newarys, aa, [pos[0]-start[0], pos[1]-start[1]], cap) {|pos2, cap2|
-      lambda { yield [pos[0]-start[0]+goal[0], pos[1]-start[1]+goal[1]], cap2 }
+    pos = st.pos
+    try_grid_rect(newarys, aa, State.new([pos[0]-start[0], pos[1]-start[1]], st.store)) {|st2|
+      lambda { yield State.new([pos[0]-start[0]+goal[0], pos[1]-start[1]+goal[1]], st2.store) }
     }
   end
 
-  def try_grid_rect(arys, aa, pos, cap)
+  def try_grid_rect(arys, aa, st)
     if arys.empty?
-      lambda { yield pos, cap }
+      lambda { yield st }
     else
       ary, *rest = arys
-      x, y = pos
+      x, y = st.pos
       pos1 = [x, y+1]
-      try_grid_row(ary, aa, pos, cap) {|pos2, cap2|
-        try_grid_rect(rest, aa, pos1, cap2) {|pos3, cap3|
-          lambda { yield pos, cap3 }
+      try_grid_row(ary, aa, st) {|st2|
+        try_grid_rect(rest, aa, State.new(pos1, st2.store)) {|st3|
+          lambda { yield State.new(st.pos, st3.store) }
         }
       }
     end
   end
 
-  def try_grid_row(ary, aa, pos, cap)
+  def try_grid_row(ary, aa, st)
     if ary.empty?
-      lambda { yield pos, cap }
+      lambda { yield st }
     else
       pat, *rest = ary
-      x, y = pos
+      x, y = st.pos
       pos1 = [x+1, y]
-      try(pat, aa, pos, cap) {|pos2, cap2|
-        try_grid_row(rest, aa, pos1, cap2) {|pos3, cap3|
-          lambda { yield pos, cap3 }
+      try(pat, aa, st) {|st2|
+        try_grid_row(rest, aa, State.new(pos1, st2.store)) {|st3|
+          lambda { yield State.new(st.pos, st3.store) }
         }
       }
     end
   end
 
-  def try_capval(n, aa, pos, cap)
-    x, y = pos
+  def try_capval(n, aa, st)
+    x, y = st.pos
     if 0 <= y && y < aa.length &&
        0 <= x && x < aa[y].length &&
       val = aa[y][x]
     else
       val = nil
     end
-    cap2 = cap.dup
-    cap2[n] = val
-    lambda { yield pos, cap2 }
+    st2 = st.merge(n => val)
+    lambda { yield st2 }
   end
 
-  def try_refval(n, aa, pos, cap)
-    x, y = pos
+  def try_refval(n, aa, st)
+    x, y = st.pos
     if 0 <= y && y < aa.length &&
-       0 <= x && x < aa[y].length &&
-      cap2 = cap.dup
+       0 <= x && x < aa[y].length
       val = aa[y][x]
     else
       val = nil
     end
-    if val == cap[n]
-      lambda { yield pos, cap2 }
+    if val == st[n]
+      lambda { yield st }
     else
       nil
     end
   end
 
-  def try_tmp_pos(dx, dy, ps, aa, pos, cap, &b)
-    x, y = pos
-    try_cat(ps, aa, [x+dx, y+dy], cap) {|pos2, cap2|
-      lambda { yield pos, cap2 }
+  def try_tmp_pos(dx, dy, ps, aa, st, &b)
+    x, y = st.pos
+    try_cat(ps, aa, State.new([x+dx, y+dy], st.store)) {|st2|
+      lambda { yield State.new(st.pos, st2.store) }
     }
   end
 
-  def try_save_pos(n, aa, pos, cap, &b)
-    cap2 = cap.dup
-    cap2[n] = pos
-    lambda { yield pos, cap2 }
+  def try_save_pos(n, aa, st, &b)
+    st2 = st.merge(n => st.pos)
+    lambda { yield st2 }
   end
 
-  def try_push_pos(n, aa, pos, cap, &b)
-    cap2 = cap.dup
-    cap2[n] ||= []
-    cap2[n] += [pos]
-    lambda { yield pos, cap2 }
+  def try_push_pos(n, aa, st, &b)
+    ary = (st[n] || []) + [st.pos]
+    st2 = st.merge(n => ary)
+    lambda { yield st2 }
   end
 
-  def try_pop_pos(n, aa, pos, cap, &b)
-    ary = cap[n]
+  def try_pop_pos(n, aa, st, &b)
+    ary = st[n]
     raise TypeError, "array expected: #{ary.inspect} (#{n.inspect})" unless Array === ary
     raise TypeError, "empty array (#{n.inspect})" if ary.empty?
     pos2 = ary.last
     raise TypeError, "array expected: #{pos2.inspect} (#{n.inspect})" unless Array === pos2
     raise TypeError, "two-element array expected: #{pos2.inspect} (#{n.inspect})" if pos2.length != 2
     raise TypeError, "integer elements expected: #{pos2.inspect} (#{n.inspect})" unless pos2.all? {|v| Integer === v }
-    cap2 = cap.dup
-    cap2[n] = ary[0...-1]
-    lambda { yield pos2, cap2 }
+    st2 = st.merge(:pos => pos2, n => ary[0...-1])
+    lambda { yield st2 }
   end
 
-  def try_update(pr, aa, pos, cap)
-    pos2, cap2 = pr.call(pos.dup, cap.dup)
-    lambda { yield pos2, cap2 }
+  def try_update(pr, aa, st)
+    st2 = pr.call(st)
+    lambda { yield st2 }
   end
 
-  def try_assert(pr, aa, pos, cap)
-    if pr.call(pos, cap)
-      lambda { yield pos, cap }
+  def try_assert(pr, aa, st)
+    if pr.call(st)
+      lambda { yield st }
     else
       nil
     end
   end
 end
+
+class Table::Pathfinder::State
+  def initialize(pos, store)
+    @pos = pos
+    @store = store.frozen? ? store : store.dup.freeze
+  end
+  attr_reader :pos, :store
+
+  def [](k)
+    if k == :pos
+      v
+    else
+      @store[k]
+    end
+  end
+
+  def merge(h)
+    return self if h.empty?
+    pos = @pos
+    store = @store
+    h.each {|k,v|
+      if k == :pos
+        pos = v
+      else
+        store = store.dup if store.frozen?
+        store[k] = v
+      end
+    }
+    store.freeze
+    Table::Pathfinder::State.new(pos, store)
+  end
+end
+
