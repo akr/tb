9d1c4eb9b178e247f92cabfecb2df080282cccf3
Tanaka Akira <akr@fsij.org>
2011-12-02 22:05:23 +0900

    move main routine.

---
+++ lib/tb/cmdmain.rb
@@ -0,0 +1,106 @@
+# Copyright (C) 2011 Tanaka Akira  <akr@fsij.org>
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#  1. Redistributions of source code must retain the above copyright notice, this
+#     list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  3. The name of the author may not be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+# OF SUCH DAMAGE.
+
+def main(argv)
+  subcommand = argv.shift
+  case subcommand
+  when 'help', '-h' then main_help(argv)
+  when 'csv' then main_csv(argv)
+  when 'tsv' then main_tsv(argv)
+  when 'json' then main_json(argv)
+  when 'yaml' then main_yaml(argv)
+  when 'pp' then main_pp(argv)
+  when 'grep' then main_grep(argv)
+  when 'gsub' then main_gsub(argv)
+  when 'sort' then main_sort(argv)
+  when 'select' then main_select(argv)
+  when 'rename' then main_rename(argv)
+  when 'newfield' then main_newfield(argv)
+  when 'cat' then main_cat(argv)
+  when 'join' then main_join(argv)
+  when 'group' then main_group(argv)
+  when 'cross' then main_cross(argv)
+  when 'shape' then main_shape(argv)
+  when 'mheader' then main_mheader(argv)
+  when 'crop' then main_crop(argv)
+  when nil
+    err "Usage: tb subcommand args..."
+  else
+    err "unexpected subcommand: #{subcommand.inspect}"
+  end
+end
+
+def usage(status)
+  print <<'End'
+Usage:
+  tb csv [OPTS] [TABLE]
+  tb tsv [OPTS] [TABLE]
+  tb json [OPTS] [TABLE]
+  tb yaml [OPTS] [TABLE]
+  tb pp [OPTS] [TABLE]
+  tb grep [OPTS] REGEXP [TABLE]
+  tb gsub [OPTS] REGEXP STRING [TABLE]
+  tb sort [OPTS] [TABLE]
+  tb select [OPTS] FIELD,... [TABLE]
+  tb rename [OPTS] SRC,DST,... [TABLE]
+  tb newfield [OPTS] FIELD RUBY-EXP [TABLE]
+  tb cat [OPTS] [TABLE ...]
+  tb join [OPTS] [TABLE ...]
+  tb group [OPTS] [TABLE]
+  tb cross [OPTS] [TABLE]
+  tb shape [OPTS] [TABLE ...]
+  tb mheader [OPTS] [TABLE]
+  tb crop [OPTS] [TABLE]
+End
+  exit status
+end
+
+def main_help(argv)
+  subcommand = argv.shift
+  case subcommand
+  when 'csv' then puts op_csv
+  when 'tsv' then puts op_tsv
+  when 'json' then puts op_json
+  when 'yaml' then puts op_yaml
+  when 'pp' then puts op_pp
+  when 'grep' then puts op_grep
+  when 'gsub' then puts op_gsub
+  when 'sort' then puts op_sort
+  when 'select' then puts op_select
+  when 'rename' then puts op_rename
+  when 'newfield' then puts op_newfield
+  when 'cat' then puts op_cat
+  when 'join' then puts op_join
+  when 'group' then puts op_group
+  when 'cross' then puts op_cross
+  when 'shape' then puts op_shape
+  when 'mheader' then puts op_mheader
+  when 'crop' then puts op_crop
+  when nil
+    usage(true)
+  else
+    err "unexpected subcommand: #{subcommand.inspect}"
+  end
+end
+
