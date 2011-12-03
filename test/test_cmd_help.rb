14854197623f303d134822d3904fa90b3ec29671
Tanaka Akira <akr@fsij.org>
2011-12-03 18:03:24 +0900

    new test flie.

---
+++ test/test_cmd_help.rb
@@ -0,0 +1,43 @@
+require 'test/unit'
+require 'tb/cmdtop'
+require 'tmpdir'
+
+class TestTbCmdHelp < Test::Unit::TestCase
+  def setup
+    Tb::Cmd.reset_option
+    @curdir = Dir.pwd
+    @tmpdir = Dir.mktmpdir
+    Dir.chdir @tmpdir
+  end
+  def teardown
+    Tb::Cmd.reset_option
+    Dir.chdir @curdir
+    FileUtils.rmtree @tmpdir
+  end
+
+  def test_basic
+    assert_equal(true, Tb::Cmd.main_help(['-o', o="msg"]))
+    msg = File.read(o)
+    assert_match(/Usage:/, msg)
+    assert_match(/ tb csv /, msg)
+    assert_match(/ tb select /, msg)
+  end
+
+  def test_subcommand
+    assert_equal(true, Tb::Cmd.main_help(['-o', o="msg", 'cat']))
+    msg = File.read(o)
+    assert_match(/tb cat /, msg)
+  end
+
+  def test_unexpected_subcommand
+    save = STDERR.dup
+    log = File.open("log", "w")
+    STDERR.reopen(log)
+    log.close
+    assert_raise(SystemExit) { Tb::Cmd.main_help(['-o', o="msg", 'foo']) }
+    STDERR.reopen(save)
+    save.close
+    assert_match(/unexpected subcommand/, File.read("log"))
+  end
+
+end
