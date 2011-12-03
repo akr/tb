5cc22784e32538fc9f48cffe7bfc8c1b5c281438
Tanaka Akira <akr@fsij.org>
2011-12-03 14:40:08 +0900

    don't show common prefix.

--- test-all-cov.rb
+++ test-all-cov.rb
@@ -13,22 +13,44 @@ end
 
 at_exit {
   r = Coverage.result
-  r.keys.sort.each {|f|
-    next if %r{lib/tb[/.]} !~ f
-    ns = r[f]
-    max = ns.compact.max
-    w = max.to_s.length
-    fmt1 = "%s %#{w}d:%s"
-    fmt2 = "%s #{" " * w}:%s"
-    File.foreach(f).with_index {|line, i|
-      line = expand_tab(line)
-      if ns[i]
-        puts fmt1 % [f, ns[i], line]
+  fs = r.keys.sort.reject {|f|
+    %r{lib/tb[/.]} !~ f
+  }
+  if !fs.empty?
+    pat = nil
+    fs[0].chars.to_a.reverse_each {|ch|
+      if !pat
+        pat = "#{Regexp.escape(ch)}?"
       else
-        puts fmt2 % [f, line]
+        pat = "(?:#{Regexp.escape(ch)}#{pat})?"
       end
     }
-  }
+    pat = Regexp.compile(pat)
+    prefix_len = fs[0].length
+    fs.each {|f|
+      l = pat.match(f).end(0)
+      prefix_len = l if l < prefix_len
+    }
+    prefix = fs[0][0, prefix_len]
+    prefix.sub!(%r{[^/]+\z}, '')
+    fs.each {|f|
+      next if %r{lib/tb[/.]} !~ f
+      f0 = f[prefix.length..-1]
+      ns = r[f]
+      max = ns.compact.max
+      w = max.to_s.length
+      fmt1 = "%s %#{w}d:%s"
+      fmt2 = "%s #{" " * w}:%s"
+      File.foreach(f).with_index {|line, i|
+        line = expand_tab(line)
+        if ns[i]
+          puts fmt1 % [f0, ns[i], line]
+        else
+          puts fmt2 % [f0, line]
+        end
+      }
+    }
+  end
 }
 Coverage.start
 
