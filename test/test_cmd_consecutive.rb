a0f5f92ccf3a4f71ebf978c0e7805430fe5ad707
Tanaka Akira <akr@fsij.org>
2011-12-24 18:01:28 +0900

    add tests.

--- test/test_cmd_consecutive.rb
+++ test/test_cmd_consecutive.rb
@@ -60,4 +60,31 @@ class TestTbCmdConsecutive < Test::Unit::TestCase
     End
   end
 
+  def test_n3
+    File.open(i="i.csv", "w") {|f| f << <<-"End".gsub(/^[ \t]+/, '') }
+      a,b,c
+      1,2,3
+      4,5,6
+      7,8,9
+    End
+    Tb::Cmd.main_consecutive(['-o', o="o.csv", '-n3', i])
+    assert_equal(<<-"End".gsub(/^[ \t]+/, ''), File.read(o))
+      a_1,a_2,a_3,b_1,b_2,b_3,c_1,c_2,c_3
+      1,4,7,2,5,8,3,6,9
+    End
+  end
+
+  def test_n4
+    File.open(i="i.csv", "w") {|f| f << <<-"End".gsub(/^[ \t]+/, '') }
+      a,b,c
+      1,2,3
+      4,5,6
+      7,8,9
+    End
+    Tb::Cmd.main_consecutive(['-o', o="o.csv", '-n4', i])
+    assert_equal(<<-"End".gsub(/^[ \t]+/, ''), File.read(o))
+      a_1,a_2,a_3,a_4,b_1,b_2,b_3,b_4,c_1,c_2,c_3,c_4
+    End
+  end
+
 end
