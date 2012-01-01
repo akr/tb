f7cb2bf04054c972049d70a074bd10de94e55236
Tanaka Akira <akr@fsij.org>
2012-01-01 17:10:10 +0900

    update copyright year.

--- lib/tb/cmd_git_log.rb
+++ lib/tb/cmd_git_log.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 Tanaka Akira  <akr@fsij.org>
+# Copyright (C) 2011-2012 Tanaka Akira  <akr@fsij.org>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
