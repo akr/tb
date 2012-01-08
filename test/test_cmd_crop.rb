944c0a7cc926588df502c8c8fdb21c8881bc335d
Tanaka Akira <akr@fsij.org>
2012-01-08 21:00:46 +0900

    update test.

--- test/test_cmd_crop.rb
+++ test/test_cmd_crop.rb
@@ -19,16 +19,16 @@ class TestTbCmdCrop < Test::Unit::TestCase
     File.open(i="i.csv", "w") {|f| f << <<-"End".gsub(/^[ \t]+/, '') }
       ,,
       ,a,b,,
-      ,0,1,,,
-      ,4,,,
+      ,0,1,,2,
+      ,,4,,,
       ,,,
 
     End
     Tb::Cmd.main_crop(['-o', o="o.csv", i])
     assert_equal(<<-"End".gsub(/^[ \t]+/, ''), File.read(o))
       a,b
-      0,1
-      4
+      0,1,,2
+      ,4
     End
   end
 
