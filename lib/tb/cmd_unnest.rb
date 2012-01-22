021cc11a6d1def1125fd5009db3c44f72957b9b7
Tanaka Akira <akr@fsij.org>
2012-01-22 16:00:03 +0900

    tb unnest don't read entire table.

--- lib/tb/cmd_unnest.rb
+++ lib/tb/cmd_unnest.rb
@@ -49,60 +49,65 @@ def (Tb::Cmd).main_unnest(argv)
   argv = ['-'] if argv.empty?
   creader = Tb::CatReader.open(argv, Tb::Cmd.opt_N)
   er = Tb::Enumerator.new {|y|
-    tbl = creader.to_tb
-    if !tbl.list_fields.include?(target_field)
-      err("field not found: #{target_field.inspect}")
-    end
-    nested_fields = []
-    tbl.each {|rec|
-      v = rec[target_field]
-      if v
-        nested_fields |= Tb.parse_csv(v).list_fields
-      end
-    }
-    if Tb::Cmd.opt_unnest_prefix
-      nested_fields.map! {|f| Tb::Cmd.opt_unnest_prefix + f }
-    end
-    result_fields = tbl.list_fields.map {|f|
-      if f == target_field
-        nested_fields.map {|nf|
-          Tb::Cmd.opt_unnest_prefix + nf
-        }
-      else
-        f
-      end
-    }.flatten
-    y.set_header result_fields
-    tbl.each {|rec|
-      if rec[target_field]
-        ntbl = Tb.parse_csv(rec[target_field])
-      end
-      if ntbl && 0 < ntbl.size
-        ntbl.each {|nrec|
-          ary = []
-          tbl.list_fields.each {|f|
-            if f == target_field
-              nested_fields.each {|nf|
-                ary << nrec[nf]
-              }
-            else
-              ary << rec[f]
-            end
-          }
-          y.yield Tb::Pairs.new(result_fields.zip(ary))
-        }
-      elsif Tb::Cmd.opt_unnest_outer
-        ary = []
-        tbl.list_fields.each {|f|
-          if f == target_field
-            nested_fields.each {|nf|
-              ary << nil
-            }
+    nested_fields = {}
+    ter = Tb::Enumerator.new {|y2|
+      creader.with_header {|header0|
+        unless header0.include? target_field
+          err("field not found: #{target_field.inspect}")
+        end
+        y2.set_header header0
+      }.each {|pairs|
+        pairs2 = {}
+        pairs.each {|f, v|
+          if f != target_field
+            pairs2[f] = v
+          elsif v.nil?
+            pairs2[f] = v
           else
-            ary << rec[f]
+            nested_tbl = Tb.parse_csv(v)
+            nested_tbl.list_fields.each {|f2|
+              unless nested_fields.has_key? f2
+                nested_fields[f2] = nested_fields.size
+              end
+            }
+            pairs2[f] = nested_tbl
           end
         }
-        y.yield Tb::Pairs.new(result_fields.zip(ary))
+        y2.yield pairs2
+      }
+    }.to_fileenumerator
+    nested_fields_ary = nested_fields.keys
+    if Tb::Cmd.opt_unnest_prefix
+      nested_fields_ary.map! {|f| Tb::Cmd.opt_unnest_prefix + f }
+    end
+    ter.with_header {|header0|
+      header2 = []
+      header0.each {|f|
+        if f != target_field
+          header2 << f
+        else
+          header2.concat nested_fields_ary
+        end
+      }
+      y.set_header header2
+    }.each {|pairs|
+      pairs2 = pairs.reject {|f, v| f == target_field }
+      ntbl = pairs[target_field]
+      if ntbl.nil? || ntbl.empty?
+        if Tb::Cmd.opt_unnest_outer
+          y.yield pairs2
+        end
+      else
+        ntbl.each {|npairs|
+          pairs3 = pairs2.dup
+          npairs.each {|nf, nv|
+            if Tb::Cmd.opt_unnest_prefix
+              nf = Tb::Cmd.opt_unnest_prefix + nf
+            end
+            pairs3[nf] = nv
+          }
+          y.yield pairs3
+        }
       end
     }
   }
