01001ce5cbb5f35db5d1a406fc1ab15c3a157c7c
Tanaka Akira <akr@fsij.org>
2012-02-11 19:58:17 +0900

    more tests.

--- test/test_cmd_git_log.rb
+++ test/test_cmd_git_log.rb
@@ -15,6 +15,16 @@ class TestTbCmdGitLog < Test::Unit::TestCase
     FileUtils.rmtree @tmpdir
   end
 
+  def with_stderr(io)
+    save = $stderr
+    $stderr = io
+    begin
+      yield
+    ensure
+      $stderr = save
+    end
+  end
+
   def test_basic
     system("git init -q")
     File.open("foo", "w") {|f| f.puts "bar" }
@@ -54,6 +64,7 @@ class TestTbCmdGitLog < Test::Unit::TestCase
     assert_equal(1, tb.size)
     assert_match(/,A,foo\n/, tb.get_record(0)["files"])
     gresult = File.read(g)
+    assert(!gresult.empty?)
     FileUtils.rmtree('.git')
     Tb::Cmd.main_git_log(['-o', o="o.csv", '--debug-git-log-input', g])
     result = File.read(o)
@@ -61,4 +72,91 @@ class TestTbCmdGitLog < Test::Unit::TestCase
     assert_equal(1, tb.size)
     assert_match(/,A,foo\n/, tb.get_record(0)["files"])
   end
+
+  def test_warn1
+    system("git init -q")
+    File.open("foo", "w") {|f| f.puts "bar" }
+    system("git add foo")
+    system("git commit -q -m msg foo")
+    Tb::Cmd.main_git_log(['-o', o="o.csv", '--debug-git-log-output', g='gitlog'])
+    result = File.read(o)
+    tb = Tb.parse_csv(result)
+    assert_equal(1, tb.size)
+    assert_match(/,A,foo\n/, tb.get_record(0)["files"])
+    gresult = File.binread(g)
+    FileUtils.rmtree('.git')
+    ###
+    gresult.sub!(/^:.*foo$/, ':hoge')
+    File.open(g, 'w') {|f| f.print gresult }
+    o2 = 'o2.csv'
+    File.open('log', 'w') {|log|
+      with_stderr(log) {
+        Tb::Cmd.main_git_log(['-o', o2, '--debug-git-log-input', g])
+      }
+    }
+    result = File.read(o2)
+    tb = Tb.parse_csv(result)
+    assert_equal(1, tb.size)
+    assert_not_match(/,A,foo\n/, tb.get_record(0)["files"])
+    log = File.read('log')
+    assert(!log.empty?)
+  end
+
+  def test_warn2
+    system("git init -q")
+    File.open("foo", "w") {|f| f.puts "bar" }
+    system("git add foo")
+    system("git commit -q -m msg foo")
+    Tb::Cmd.main_git_log(['-o', o="o.csv", '--debug-git-log-output', g='gitlog'])
+    result = File.read(o)
+    tb = Tb.parse_csv(result)
+    assert_equal(1, tb.size)
+    assert_match(/,A,foo\n/, tb.get_record(0)["files"])
+    gresult = File.binread(g)
+    FileUtils.rmtree('.git')
+    ###
+    gresult.sub!(/^author-name:/, 'author-name ')
+    File.open(g, 'w') {|f| f.print gresult }
+    o2 = 'o2.csv'
+    File.open('log', 'w') {|log|
+      with_stderr(log) {
+        Tb::Cmd.main_git_log(['-o', o2, '--debug-git-log-input', g])
+      }
+    }
+    result = File.read(o2)
+    tb = Tb.parse_csv(result)
+    assert_equal(1, tb.size)
+    assert_match(/,A,foo\n/, tb.get_record(0)["files"])
+    log = File.read('log')
+    assert(!log.empty?)
+  end
+
+  def test_warn3
+    system("git init -q")
+    File.open("foo", "w") {|f| f.puts "bar" }
+    system("git add foo")
+    system("git commit -q -m msg foo")
+    Tb::Cmd.main_git_log(['-o', o="o.csv", '--debug-git-log-output', g='gitlog'])
+    result = File.read(o)
+    tb = Tb.parse_csv(result)
+    assert_equal(1, tb.size)
+    assert_match(/,A,foo\n/, tb.get_record(0)["files"])
+    gresult = File.binread(g)
+    FileUtils.rmtree('.git')
+    ###
+    gresult.sub!(/end-commit/, 'endcommit')
+    File.open(g, 'w') {|f| f.print gresult }
+    o2 = 'o2.csv'
+    File.open('log', 'w') {|log|
+      with_stderr(log) {
+        Tb::Cmd.main_git_log(['-o', o2, '--debug-git-log-input', g])
+      }
+    }
+    result = File.read(o2)
+    tb = Tb.parse_csv(result)
+    assert_equal(0, tb.size)
+    log = File.read('log')
+    assert(!log.empty?)
+  end
+
 end
