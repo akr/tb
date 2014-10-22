415b4d0472cc017818df037e4dac5addc6713686
Tanaka Akira <akr@fsij.org>
2014-10-22 23:43:14 +0900

    csv tests rewritten.

--- test/test_csv.rb
+++ test/test_csv.rb
@@ -2,95 +2,55 @@ require 'tb'
 require 'test/unit'
 
 class TestTbCSV < Test::Unit::TestCase
-=begin
+  def parse_csv(csv)
+    Tb::HeaderCSVReader.new(StringIO.new(csv)).to_a
+  end
+
+  def generate_csv(ary)
+    writer = Tb::HeaderCSVWriter.new(out = '')
+    ary.each {|h| writer.put_hash h }
+    writer.finish
+    out
+  end
+
   def test_parse
-    csv = <<-'End'.gsub(/^\s+/, '')
+    t = parse_csv(<<-'End'.gsub(/^\s+/, ''))
     a,b
     1,2
     3,4
     End
-    t = Tb.parse_csv(csv)
-    records = []
-    t.each_record {|record|
-      records << record.to_h_with_reserved
-    }
     assert_equal(
-      [{"_recordid"=>0, "a"=>"1", "b"=>"2"},
-       {"_recordid"=>1, "a"=>"3", "b"=>"4"}],
-      records)
+      [{"a"=>"1", "b"=>"2"},
+       {"a"=>"3", "b"=>"4"}],
+      t)
   end
 
   def test_parse_empty
-    csv = <<-'End'.gsub(/^\s+/, '')
+    t = parse_csv(<<-'End'.gsub(/^\s+/, ''))
     a,b,c
     1,,2
     3,"",4
     End
-    t = Tb.parse_csv(csv)
-    records = []
-    t.each_record {|record|
-      records << record.to_h_with_reserved
-    }
     assert_equal(
-      [{"_recordid"=>0, "a"=>"1", "c"=>"2"},
-       {"_recordid"=>1, "a"=>"3", "b"=>"", "c"=>"4"}],
-      records)
+      [{"a"=>"1", "b"=>nil, "c"=>"2"},
+       {"a"=>"3", "b"=>"", "c"=>"4"}],
+      t)
   end
-
-  def test_parse_conv
-    csv = "foo\na,b\n1,2\n"
-    t = Tb.parse_csv(csv) {|aa|
-      assert_equal([%w[foo],
-                   %w[a b],
-                   %w[1 2]],
-                   aa)
-      aa.shift
-      aa
-    }
-    records = []
-    t.each_record {|record|
-      records << record.to_h_with_reserved
-    }
-    assert_equal(
-      [{"_recordid"=>0, "a"=>"1", "b"=>"2"}],
-      records)
-  end
-=end
 
   def test_generate
-    t = Tb.new %w[a b], [1, 2], [3, 4]
-    out = t.generate_csv('', ['a', 'b'])
-    assert_equal(<<-'End'.gsub(/^\s+/, ''), out)
+    t = [{'a' => 1, 'b' => 2},
+         {'a' => 3, 'b' => 4}]
+    assert_equal(<<-'End'.gsub(/^\s+/, ''), generate_csv(t))
     a,b
     1,2
     3,4
-    End
-  end
-
-  def test_generate_without_explicit_fields
-    t = Tb.new %w[a b], [1, 2], [3, 4]
-    out = t.generate_csv('')
-    assert_equal(<<-'End'.gsub(/^\s+/, ''), out)
-    a,b
-    1,2
-    3,4
-    End
-  end
-
-  def test_generate_with_block
-    t = Tb.new %w[a b], [1, 2], [3, 4]
-    out = t.generate_csv('', ['a', 'b']) {|recids| recids.reverse }
-    assert_equal(<<-'End'.gsub(/^\s+/, ''), out)
-    a,b
-    3,4
-    1,2
     End
   end
 
   def test_generate_empty
-    t = Tb.new %w[a b c], [1, nil, 2], [3, '', 4]
-    out = t.generate_csv('', ['a', 'b', 'c'])
-    assert_equal(<<-'End'.gsub(/^\s+/, ''), out)
+    t = [{'a' => 1, 'b' => nil, 'c' => 2},
+         {'a' => 3, 'b' => '',  'c' => 4}]
+    assert_equal(<<-'End'.gsub(/^\s+/, ''), generate_csv(t))
     a,b,c
     1,,2
     3,"",4
